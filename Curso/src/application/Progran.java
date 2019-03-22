b6a27da8335cdae672dca8f50e6c8c5e40f4768e
blackbarth <blackbarth@outlook.com>
2019-03-22 01:38:23 -0300

    Projeto Java
    Signed-off-by: blackbarth <blackbarth@outlook.com>

---
+++ Curso/src/application/Progran.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Scanner;
+
+public class Progran {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Entre com valor para impressão: ");
+		int x = sc.nextInt();
+		System.out.printf("Teste de programa %d",x);
+		sc.close();
+	}
+
+}
